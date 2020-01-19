1a61bb689fc1f5ef0c97024d7c037b10dc8d15c4
Daniel Mundra <dmundra@uoregon.edu>
2020-01-19 14:23:45 -0800

    Added URL parameters to new map from https://blog.mastermaps.com/2012/10/how-to-control-your-leaflet-map-with.html.

--- galizien-und-nordost-ungarn.js
+++ galizien-und-nordost-ungarn.js
@@ -1,10 +1,16 @@
 var allgemeinechartedeskoenigreiches_baseoverlay = null;
 
 $(document).ready(function() {
+  // Get url parameters
+  var params = {};
+  window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+    params[key] = value;
+  });
+
   // Initialize the map on the "map" div with a given center and zoom.
   var map = L.map('map', {
-    center: [49.3642, 24.5900],
-    zoom: 7
+    center: [params.lat || 49.3642, params.lng || 24.5900],
+    zoom: params.zoom || 7
   });
 
   var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -68,14 +74,6 @@ $(document).ready(function() {
   var overlayMaps = {
     "General": {
       "Galizien und Nordost-Ungarn, Carl Flemming ~ 1916": galizienundnordostungarn_baseoverlay,
-    },
-    "Views of Galicia": {
-      "Austrian Galicia 1772": geojsonLayer1,
-      "Austrian Bukovina 1775": geojsonLayer2,
-      "West Galicia 1795-1809": geojsonLayer3,
-      "Removal of the Tarnopol Region 1809-1815": geojsonLayer4,
-      "Free State of Krakow 1815-1846": geojsonLayer5,
-      "Austrian Galicia and Bukovina 1846-1918": geojsonLayer6,
     },
   };
 
