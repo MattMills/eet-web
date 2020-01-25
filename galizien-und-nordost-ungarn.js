00e01dbe1cb0f56b16b490e3a8f2ddc3ad637069
easteurotopo <easteurotopo@riousa.com>
2020-01-25 12:06:34 -0600

    Tweaked geojon for galician map.

--- galizien-und-nordost-ungarn.js
+++ galizien-und-nordost-ungarn.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
     "weight": 3,
   };
 
-  var geojsonLayer1 = new L.GeoJSON.AJAX("geojson/jewish_communities_of_galicia_1916_xxo_2.json", { style: style2 });
+  var geojsonLayer1 = new L.GeoJSON.AJAX("geojson/jewish_communities_of_galicia_1916_xxo.json").addTo(map);
 
   var baseMaps = {
     "Stamen Toner": stamen_toner,
